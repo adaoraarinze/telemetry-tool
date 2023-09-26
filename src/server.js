4487dd585eccdc5be4502230de1c06b894be7bbd
Adaora Arinze <adaoraarinze2003@gmail.com>
2023-09-26 12:09:06 +0100

    successful post request

--- src/server.js
+++ src/server.js
@@ -7,8 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = 3000;
-app.get('/', (req, res) => res.send('waiting for data...'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/', (req, res) => { 
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/", (req, res) => {
+    res.send("received data");
+  });
+
+
 /*
 app.post('/data', (req, res) => {
     console.log(req.body);
